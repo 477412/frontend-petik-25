02d43d892c64fa15a700d2bd2892e00ff7082dcd
sntri_z <ahmadfaisalamar11@gmail.com>
2026-01-10 20:54:59 +0700

    Update App.jsx dan Navbar.css

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import Content from "./components/Content/Content";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 
+
 function App() {
   return (
+ 
     <>
       <Navbar />
       <Content nama={"Ucup"} membership={"Platinum"} />
